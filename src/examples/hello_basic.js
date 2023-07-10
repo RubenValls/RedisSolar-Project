9941fe0a34c35377c24ab9d0a84d8bc2912760c8
Rubén Valls <r.vallsaparici@gmail.com>
2023-07-10 17:45:22 +0200

    changed the logs to comments.

--- src/examples/hello_basic.js
+++ src/examples/hello_basic.js
@@ -12,7 +12,7 @@ client.set('hello', 'world', (err, reply) => {
   //After a set function we will get the response of the server:
   console.log(reply); // OK
 
-  // Run a second Redis command now we know that the
+  // Run a second Redis command now we know that the 
   // first one completed.  Again, response in callback.
   client.get('hello', (getErr, getReply) => {
     //'We can call another function to get the information inside the set function:
